278d7b10a809ef0f6cc9289014a72fffba927e1c
Varsha Aggarwal <aggarwalvarsha19@gmail.com>
2020-08-24 23:41:07 +0530

    test

--- expensetracker/Server/server.js
+++ expensetracker/Server/server.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const path = require('path'); 
 
 dotenv.config({path: './config/config.env'});
 
@@ -21,8 +21,14 @@ app.use(express.json());
 if(process.env.NODE_ENV === "development"){
     app.use(morgan("dev"));
 }
+app.use(express.static(path.join(__dirname, "client/build")));
 app.use('/api/v1/transactions', transactions);
 app.use('/',authRoutes);
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + "client/build.index.html"));
+})
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on  PORT: ${PORT}`));
